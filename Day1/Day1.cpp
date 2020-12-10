b398363ded255f5b6fe6e225a6931dbb6fb4c564
Infaninio <infaninio@gmail.com>
2020-12-10 15:01:27 +0100

    Added Day1 and Day2. Project contains a CMakeSettings.json which is usefull inside Visual Studio 2019

---
+++ Day1/Day1.cpp
@@ -0,0 +1,41 @@
+﻿// Day1.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "Day1.h"
+
+using namespace std;
+
+int main()
+{
+	// Aufgabe 1
+	//for (auto var : input)
+	//{
+	//	for (auto var2 : input)
+	//	{
+	//		if (2020 == (var+var2))
+	//		{
+	//			std::cout << "Die Loesung ist: " << (var * var2) << "\n";
+	//			return 0;
+	//		}
+	//	}
+	//}
+
+
+	//Part2
+	for (auto var : input)
+	{
+		for (auto var2 : input)
+		{
+			for (auto var3 : input)
+			{
+				if (2020 == (var+var2+var3))
+				{
+					std::cout << "Solution Part2: " << (var * var2 * var3) << "\n";
+					return 0;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ Day2/Day2.cpp
@@ -0,0 +1,38 @@
+﻿// Day1.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "Day2.h"
+
+using namespace std;
+
+int main()
+{
+	int correctPW = 0;
+	
+	
+	// Part 1
+	/*for (auto var : input)
+	{
+		int numOfChar = 0;
+		for each (auto letter in var.password)
+		{
+			if (var.letter == letter)
+				numOfChar++;
+		}
+
+		if (numOfChar >= var.minNumOfAppearance && numOfChar <= var.maxNumOfAppeareance)
+			correctPW++;
+
+	}*/
+
+
+	//Part 2
+	for (auto pwContainer : input)
+	{
+		if (!(pwContainer.letter == pwContainer.password.at(pwContainer.minNumOfAppearance - 1)) != !(pwContainer.letter == pwContainer.password.at(pwContainer.maxNumOfAppeareance - 1)))
+			correctPW++;
+	}
+
+
+	std::cout << correctPW << " correct Passwords";
+}
